4fef86d4763eec1835b5201880e96ba50a12170e
Harco Kuppens <h.kuppens@gmail.com>
2020-05-05 16:52:55 +0200

    moved toolversion file to more logical place and uppercase name

--- source/conf.py
+++ source/conf.py
@@ -28,7 +28,7 @@ author = 'Harco Kuppens'
 # This adds 'toolversion' as variable to sphinx documentation.
 # Use in .rst file as |toolversion|
 # eg. Documentation for TorXakis version: |toolversion|
-with open('../toolversion.txt') as f:
+with open('TOOLVERSION.txt') as f:
     toolversion = f.readline()
     rst_epilog = '.. |toolversion| replace:: %s' % toolversion
 
