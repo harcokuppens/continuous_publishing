7d64ef3fe88a877506c6a8edd2d20ad8bd0adf6c
GitHub Action <action@github.com>
2020-05-23 23:08:29 +0000

    Update documentation

--- stable/_static/documentation_options.js
+++ stable/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v4.3.14, Document version git-sha1-e54c14a',
+    VERSION: 'v4.3.14, Document version v0.1.21',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
